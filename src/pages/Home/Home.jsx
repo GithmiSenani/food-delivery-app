b97d48f4da40616de516aa0922c0f1e00d2b745d
GithmiSenani <135831931+GithmiSenani@users.noreply.github.com>
2025-04-25 00:44:27 +0530

    fixed view menu button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
+import ScrollToTopButton from '../../components/ScrollToTopButton/ScrollToTopButton' // adjust path based on your structure
 
 const Home = () => {
 
@@ -15,6 +16,7 @@ const Home = () => {
      <ExploreMenu category={category} setCategory={setCategory}/> {/* Passing category state and setter function to ExploreMenu */}
      <FoodDisplay category={category}/> {/* Passing selected category to FoodDisplay */}
      <AppDownload/> {/* Rendering AppDownload component */}
+     <ScrollToTopButton /> {/* Add scroll-to-top button here */}
    </div>
  )
 }
